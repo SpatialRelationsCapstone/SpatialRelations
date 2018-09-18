ae5d05aa79272856e7862ef7f0285f4bdd552b0f
rayheberer <ray.heberer@gmail.com>
2018-09-18 12:42:16 -0700

    Add generate_anchor_coordinates function

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -1,5 +1,7 @@
 """Data preprocessing operations."""
 
+import numpy as np
+
 from skimage import resize
 
 
@@ -10,16 +12,50 @@ def resize_by_shorter(image, annotation, shorter_length=600, mode="reflect"):
     shorter = min(height, width)
     longer = max(height, width)
 
-    r = shorter_length / shorter
-    longer_length = int(longer * r)
+    ratio = shorter_length / shorter
+    longer_length = int(longer * ratio)
 
     if height > width:
         resized = resize(image, (longer_length, shorter_length), mode=mode)
     else:
         resized = resize(image, (shorter_length, longer_length), mode=mode)
 
-    for rel in annotation:
-        rel["object"]["bbox"] = [int(n * r) for n in rel["object"]["bbox"]]
-        rel["subject"]["bbox"] = [int(n * r) for n in rel["subject"]["bbox"]]
+    annotation = annotation.copy()
+    for a in annotation:
+        a["object"]["bbox"] = [int(n * ratio) for n in a["object"]["bbox"]]
+        a["subject"]["bbox"] = [int(n * ratio) for n in a["subject"]["bbox"]]
 
     return resized, annotation
+
+
+def generate_anchor_coordinates(image,
+                                stride=16,
+                                sides=[128, 256, 512],
+                                ratios=[0.5, 1, 2]):
+    """Generate anchors to serve as regression targets."""
+    imheight, imwidth, _ = image.shape
+
+    # get anchor center coordinates
+    y = np.arange(0, imheight, stride)
+    x = np.arange(0, imwidth, stride)
+
+    yv, xv = np.meshgrid(y, x)
+
+    y_ctr = np.broadcast_to(yv, (len(sides) * len(ratios), *yv.shape)).T
+    x_ctr = np.broadcast_to(xv, (len(sides) * len(ratios), *xv.shape)).T
+
+    # get anchor heights and widths
+    h = []
+    w = []
+    for side in sides:
+        for ratio in ratios:
+            h.append(int(side * np.sqrt(ratio)))
+            w.append(int(side / np.sqrt(ratio)))
+
+    heights = np.broadcast_to(h, (len(y), len(x), len(h)))
+    widths = np.broadcast_to(w, (len(y), len(x), len(w)))
+
+    # stack together so that the last dimension matches RPN output
+    anchors = np.stack([y_ctr, x_ctr, heights, widths], axis=-1)
+    anchors = anchors.reshape(*y_ctr.shape[:2], 4 * len(sides) * len(ratios))
+    return anchors
